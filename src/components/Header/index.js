80f019623572bdfc106d9577d9dd69a484955b37
Vendula Krajíčková <vendysacek@seznam.cz>
2023-07-20 05:52:49 +0200

    header box-shadow

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,18 +6,20 @@ export const Header = () => {
   const element = document.createElement('header');
   element.innerHTML = `
     <div class="header">
-      <img class="header__logo" src="${logoWine}" width="150" height="80" alt="logo Winery" />
+      <div class="header__inner">
+        <img class="header__logo" src="${logoWine}" width="150" height="80" alt="logo Winery" />
+      </div>
     </div>
   `;
 
-  element.append(Navigation());
+  element.querySelector('.header').append(Navigation());
 
-  element.querySelector('.header').addEventListener('click', () => {
+  element.querySelector('.header__inner').addEventListener('click', () => {
     window.location.href = '/';
   });
 
-  element.querySelector('.header').addEventListener('mouseover', () => {
-    element.querySelector('.header').style.cursor = 'pointer';
+  element.querySelector('.header__inner').addEventListener('mouseover', () => {
+    element.querySelector('.header__inner').style.cursor = 'pointer';
   });
 
   return element;
